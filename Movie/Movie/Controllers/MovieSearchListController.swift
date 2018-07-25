e72e256e53363a650c9bbf25c094b115cce33ea7
Sasi M <msasi7274@gmail.com>
2018-07-25 21:11:30 +0530

    - MovieList controller rename into MovieSearchList Controller - Storyboard design for MovieSearchListController - IBOutlet references creation for MovieSearchListController - TableView datasource/delegate and SearchBar delegate extensions inclusion

---
+++ Movie/Movie/Controllers/MovieSearchListController.swift
@@ -0,0 +1,62 @@
+//
+//  MovieListController.swift
+//  Movie
+//
+//  Created by Sasi M on 25/07/18.
+//  Copyright © 2018 Sasi. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchListController: UIViewController {
+
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    // MARK: - Movie data fetch
+    
+}
+
+extension MovieSearchListController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+}
+
+extension MovieSearchListController: UITableViewDataSource {
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+}
+
+extension MovieSearchListController: UITableViewDelegate {
+    
+}
+
+
+
