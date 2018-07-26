61a839bed1e2af4ac836d75d0c433e741951405e
Sasi M <msasi7274@gmail.com>
2018-07-26 11:44:32 +0530

    - MovieDataHandler class creation

---
+++ Movie/Movie/MovieDataHandler.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDataHandler.swift
+//  Movie
+//
+//  Created by Sasi M on 25/07/18.
+//  Copyright © 2018 Sasi. All rights reserved.
+//
+
+import Foundation
+
+class MovieDataHandler {
+    
+}
